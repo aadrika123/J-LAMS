93290deec535e8e919e4909b43b06359c15eba79
root <it.info@aadrikaenterprises.com>
2025-02-03 19:26:53 +0530

    add the changes

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,12 +5,12 @@ const nextConfig = {
   basePath: "/lams",
   env: {
     // backend: "http://localhost:8000",
-<<<<<<< HEAD
+
     backend: "https://jharkhandegovernance.com/auth",
-=======
-    backend: "https://aadrikainfomedia.com/auth",
+
+//    backend: "https://aadrikainfomedia.com/auth",
     // backend: "https://jharkhandegovernance.com/auth",
->>>>>>> refs/remotes/origin/master
+ 
   },
 
 };
