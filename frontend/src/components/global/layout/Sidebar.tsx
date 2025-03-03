930cdd2e5f6099d762bd77014906429d3c01abd4
sahil raj <sahil27raj@gmail.com>
2025-03-03 18:24:39 +0530

    reverse changes for aadrika server

--- frontend/src/components/global/layout/Sidebar.tsx
+++ frontend/src/components/global/layout/Sidebar.tsx
@@ -236,8 +236,8 @@ const SideBar: React.FC<SideBarProps> = () => {
     try {
       // Make API request
       const res = await axios.post(
-        // `https://aadrikainfomedia.com/auth/api/menu/by-module`,
-        `https://jharkhandegovernance.com/auth/api/menu/by-module`,
+        `https://aadrikainfomedia.com/auth/api/menu/by-module`,
+        // `https://jharkhandegovernance.com/auth/api/menu/by-module`,
         // `https://egov.rsccl.in/auth/api/menu/by-module`,
         requestBody,
         {
