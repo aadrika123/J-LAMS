fed1ec0b1e83f869db35d469cb21926e74e1ea89
sahil raj <sahil27raj@gmail.com>
2025-03-03 18:15:13 +0530

    add the changes for stagiing build for jharkhand

--- frontend/src/components/hooks/useModulePermission.jsx
+++ frontend/src/components/hooks/useModulePermission.jsx
@@ -22,12 +22,10 @@ const useModulePermission = () => {
     axios
       .post(
 
-        `https://aadrikainfomedia.com/auth/api/menu/by-module`,
-        // `https://jharkhandegovernance.com/auth/api/menu/by-module`,
+        // `https://aadrikainfomedia.com/auth/api/menu/by-module`,
+        `https://jharkhandegovernance.com/auth/api/menu/by-module`,
         // `https://egov.rsccl.in/auth/api/menu/by-module`,
 
-        // `https://aadrikainfomedia.com/auth/api/menu/by-module`,
-        // `https://jharkhandegovernance.com/auth/api/menu/by-module`,
 
         requestBody,
         {
