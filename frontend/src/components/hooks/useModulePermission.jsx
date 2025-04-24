4f9d7373c356856393d285aa4ff1961509d69acb
sahil raj <sahil27raj@gmail.com>
2025-04-24 18:20:43 +0530

    add the changes for aadrika server

--- frontend/src/components/hooks/useModulePermission.jsx
+++ frontend/src/components/hooks/useModulePermission.jsx
@@ -22,9 +22,9 @@ const useModulePermission = () => {
     axios
       .post(
 
-        // `https://aadrikainfomedia.com/auth/api/menu/by-module`,
+        `https://aadrikainfomedia.com/auth/api/menu/by-module`,
         // `https://jharkhandegovernance.com/auth/api/menu/by-module`,
-        `https://egov.rsccl.in/auth/api/menu/by-module`,
+        // `https://egov.rsccl.in/auth/api/menu/by-module`,
 
 
         requestBody,
