466f5a9af13ce47a0f07eaa3ab3feef83b19ebd6
sahil raj <sahil27raj@gmail.com>
2025-06-05 12:25:45 +0530

    add the changes here for handling the content issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,17 +1,50 @@
 
 
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: "/lams",
+//   env: {
+//     // backend: "http://localhost:8000",
+//     backend: "https://jharkhandegovernance.com/auth",
+//     // backend: "https://aadrikainfomedia.com/auth",
+//     // backend: "https://egov.rsccl.in/auth" 
+
+//         // backend: "http://172.30.48.1:2000",
+//   },
+
+// };
+
+// module.exports = nextConfig;
+
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/lams",
   env: {
-    // backend: "http://localhost:8000",
+// backend: "http://localhost:8000",
     backend: "https://jharkhandegovernance.com/auth",
-    // backend: "https://aadrikainfomedia.com/auth",
-    // backend: "https://egov.rsccl.in/auth" 
+//     // backend: "https://aadrikainfomedia.com/auth",
+//     // backend: "https://egov.rsccl.in/auth" 
 
-        // backend: "http://172.30.48.1:2000",
+//         // backend: "http://172.30.48.1:2000",
   },
 
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Applies to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'none'; base-uri 'self';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
